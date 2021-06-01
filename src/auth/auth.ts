7aa29ecaaf4d8a23f4e6a4a598dcbf29b5c37aad
TmLololol <tmtanky@gmail.com>
2021-06-01 23:03:24 +0800

    trying to fix again

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,62 +1,86 @@
 import {NextFunction, Request, RequestHandler, Response} from 'express'
 import {verify} from 'jsonwebtoken'
 
-export const auth: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+// export const auth: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
 
-    let token
+//     let token
 
-    try {
+//     try {
 
-        // if (!req.isAuth) {
-        //     throw new Error ('Unauthorized')
-        // }
+//         // if (!req.isAuth) {
+//         //     throw new Error ('Unauthorized')
+//         // }
 
-        if (req.headers.login === 'true') {
-            req.isAuth = false
-            return next()
-        }
+//         if (req.headers.login === 'true') {
+//             req.isAuth = false
+//             return next()
+//         }
 
-        if (req.headers.register === 'true') {
-            req.isAuth = false
-            return next()
-        }
+//         if (req.headers.register === 'true') {
+//             req.isAuth = false
+//             return next()
+//         }
 
-        if (req.headers.reset === 'true') {
-            req.isAuth = false
-            return next()
-        }
+//         if (req.headers.reset === 'true') {
+//             req.isAuth = false
+//             return next()
+//         }
 
-        if (req.headers.confirm === 'true') {
-            req.isAuth = false
-            return next()
-        }
+//         if (req.headers.confirm === 'true') {
+//             req.isAuth = false
+//             return next()
+//         }
 
-        // if (!req.isAuth) {
-        //     throw new Error ('Unauthoasdadrized')
-        // }
+//         // if (!req.isAuth) {
+//         //     throw new Error ('Unauthoasdadrized')
+//         // }
 
-        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-            token = req.headers.authorization.split(" ")[1]
+//         if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+//             token = req.headers.authorization.split(" ")[1]
 
-            const decoded = verify(token, process.env.JWT_KEY as string)
+//             const decoded = verify(token, process.env.JWT_KEY as string)
     
-            if (decoded) {
-                // console.log(decoded)
-                req.isAuth = true
-                return next()
-            }
+//             if (decoded) {
+//                 // console.log(decoded)
+//                 req.isAuth = true
+//                 return next()
+//             }
     
-        }
+//         }
     
-        // if (!token) {
-        //     throw new Error ('Invalid')
-        // }
+//         // if (!token) {
+//         //     throw new Error ('Invalid')
+//         // }
 
-        return next()
+//         return next()
         
-    } catch (err) {
-        console.log(err)
-        return err
+//     } catch (err) {
+//         console.log(err)
+//         return err
+//     }
+
+// }
+
+export const auth: RequestHandler = (req, res, next) => {
+
+    let token 
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+
+        token = req.headers.authorization.split(' ')[1]
+
+        const decoded = verify(token, process.env.JWT_KEY as string)
+
+        if (decoded) {
+            return next()
+        } else {
+            throw new Error ('Invalid Token')
+        }
+
+    } 
+
+    if (!token) {
+        throw new Error ('Invalid Token')
     }
 
 }
